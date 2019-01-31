cb121aab34d65063a374628032f906302ed7634f
PerryP1 <perryapublico@gmail.com>
2019-01-31 16:26:36 -0500

    initial commit

---
+++ src/main/java/com/example/demo/AverageScoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AverageScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AverageScoreApplication.class, args);
+    }
+
+}
+
